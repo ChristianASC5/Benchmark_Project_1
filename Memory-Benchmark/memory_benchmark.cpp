9901c5df054aa59c70435c510715cf4e4f3b8b88
ChristianASC5 <cnicolas3411@gmail.com>
2023-02-07 15:11:46 -0500

    Added method to run 32-bit integer calculations. Added loop for additions of floating point constants.

--- Memory-Benchmark/memory_benchmark.cpp
+++ Memory-Benchmark/memory_benchmark.cpp
@@ -5,3 +5,14 @@ using namespace std;
 using namespace std::chrono;
 
 #define ARRAY_SIZE 1000000000
+
+void MemoryBenchmark(){
+    int *nums = new int[ARRAY_SIZE];
+    int num, i, j;
+
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < ARRAY_SIZE; j++){
+            num = *(nums + j);
+        }
+    }
+}
