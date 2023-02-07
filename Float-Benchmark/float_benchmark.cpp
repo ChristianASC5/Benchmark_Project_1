ff17235d7a7d03533a8275ce6a2a37765d7acb36
ChristianASC5 <cnicolas3411@gmail.com>
2023-02-07 17:16:00 -0500

    Added floating point constant addition operations.

--- Float-Benchmark/float_benchmark.cpp
+++ Float-Benchmark/float_benchmark.cpp
@@ -4,4 +4,12 @@
 using namespace std;
 using namespace std::chrono;
 
-void RunCalculations(){}
+void RunCalculations(){
+    int result;
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < pow(10,9); j++){
+            result = 1.0 + 1.0;
+        }
+    }    
+}
